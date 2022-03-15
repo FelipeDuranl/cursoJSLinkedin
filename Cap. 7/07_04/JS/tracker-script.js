9bd131dfe148467629da52a2187d614a658995aa
Felipe <felipeduram123@gmail.com>
2022-03-14 22:46:27 -0300

    criação de outro circulo

--- Cap. 7/07_04/JS/tracker-script.js
+++ Cap. 7/07_04/JS/tracker-script.js
@@ -1,8 +1,11 @@
 const AREA = document.body;
 const CIRCLE = document.querySelector('.circle');
+const CIRCLEDOIS = document.querySelector('.circleDois');
 
 var windowWidth = window.innerWidth;
 var windowHeight = window.innerHeight;
+var windowWidthDois = window.innerWidth;
+var windowHeightDois = window.innerHeight;
 
 function mouseCoordinates(e) {
     // Captura o objeto de evento (movimento do mouse).
@@ -17,16 +20,34 @@ function mouseCoordinates(e) {
     CIRCLE.style.top = verticalPosition + 'px';
 }
 
+function mouseCoordinatesDois(e){
+    var horizontalPositionDois = windowWidthDois - e.clientX - 100;
+    var verticalPositionDois = windowHeightDois - e.clientY-100;
+
+    CIRCLEDOIS.style.left = horizontalPositionDois + 'px';
+    CIRCLEDOIS.style.top = verticalPositionDois + 'px';
+}
+
 function changeColorOnTouch() {
     CIRCLE.style.backgroundColor = "green";
     CIRCLE.style.borderColor = "green";
 }
 
+function changeColorOnTouchDois(){
+
+    CIRCLEDOIS.style.backgroundColor = "green";
+    CIRCLEDOIS.style.borderColor = "green";
+
+}
+
 // Quando o mouse se move, executa a função mouseCoordinates.
 AREA.addEventListener('mousemove', mouseCoordinates, false);
+AREA.addEventListener('mousemove', mouseCoordinatesDois, false);
 
 // Quando o mouse toca o círculo, executa a função changeColorOnTouch.
 CIRCLE.addEventListener('mouseenter', changeColorOnTouch, false);
+CIRCLEDOIS.addEventListener('mouseenter', changeColorOnTouchDois, false);
 
 // Quando o mouse sai do círculo, remove estilos embutidos com uma função anônima.
 CIRCLE.addEventListener('mouseleave', function(){CIRCLE.removeAttribute("style");}, false);
+CIRCLEDOIS.addEventListener('mouseleave', function(){CIRCLEDOIS.removeAttribute("style");}, false);
